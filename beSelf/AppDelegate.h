5e0af8adeae4ed27daa5a15c7370df0e8c435c18
木 <ki@mudeiMac.local>
2015-04-08 09:21:58 +0800

    Initial Commit

---
+++ beSelf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  beSelf
+//
+//  Created by 木 on 15/4/8.
+//  Copyright (c) 2015年 木. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ beSelf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  beSelf
+//
+//  Created by 木 on 15/4/8.
+//  Copyright (c) 2015年 木. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
